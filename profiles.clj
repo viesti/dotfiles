807258390cde8d4438a90ce005d733ca584c9836
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2020-04-23 13:05:52 +0300

    Synch profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,8 @@
 {:non-pedantic {:pedantic? :ranges}
  :vizdeps {:plugins [[walmartlabs/vizdeps "0.1.6"]]}
- :shared {:plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]
+ :shared {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
                     [lein-pprint "1.1.2"]]
           :figwheel {:open-file-command "figwheel-file-opener"}
-          :test-refresh {:growl true}
           #_#_:source-paths ["/users/kimmoko/.clojure/user"]}
  :global-opts {:global-vars {*print-length* 100}}
  :ancient {:plugins [[lein-ancient "0.6.15"]]}
@@ -18,18 +17,29 @@
                                 [clojure.repl doc source])
                                (require 'pjstadig.humane-test-output)
                                (pjstadig.humane-test-output/activate!)]}
- :moar-opts {:jvm-opts ["-XX:+UnlockDiagnosticVMOptions"
-                        "-XX:+DebugNonSafepoints"
-                        "-XX:+UnlockCommercialFeatures"
-                        "-XX:+FlightRecorder"]}
+ :jvm-opts {:jvm-opts [;; Keep stacktraces
+                       "-XX:-OmitStackTraceInFastThrow"]}
  :yagni {:plugins [[venantius/yagni "0.1.2"]]}
  :line-count {:plugins [[lein-count "1.0.9"]]}
- :cider {:plugins [[cider/cider-nrepl "0.21.0"]
-                   [refactor-nrepl "2.4.0"]]}
- :user [:shared :global-opts :moar-opts :injected-tools]
+ :cider {:plugins [[cider/cider-nrepl "0.24.0"]
+                   [refactor-nrepl "2.5.0"]]}
  :wait-debugger {:jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
                             "-Djava.rmi.server.hostname=127.0.0.1"]}
  :clojars-release {:deploy-repositories [["releases" {:url "https://clojars.org/repo/"
                                                       :password :gpg
                                                       :username :gpg}]]}
- :eastwood {:plugins [[jonase/eastwood "0.3.5"]]}}
+ :eastwood {:plugins [[jonase/eastwood "0.3.5"]]}
+ :clj-async-profiler {:dependencies [[com.clojure-goes-fast/clj-async-profiler "0.4.0"]]
+                      :jvm-opts ["-XX:+UnlockDiagnosticVMOptions"
+                                 "-XX:+DebugNonSafepoints"]}
+ :nrebl  {:repl-options {:nrepl-middleware [nrebl.middleware/wrap-nrebl]}
+          :dependencies [[rickmoynihan/nrebl.middleware "0.3.1"]
+                         [org.clojure/core.async "0.4.490"]
+                         [cljfmt "0.6.4"]]
+          :resource-paths ["/Users/kimmoko/home/rebl/REBL-0.9.220/REBL-0.9.220.jar"]
+          :injections [(require 'nrebl.middleware) (require '[cognitect.rebl :as rebl])]}
+ :honeysql {:dependencies [[honeysql "0.9.8"]
+                           [nilenso/honeysql-postgres "0.2.6"]]}
+ :hashp {:dependencies [[hashp "0.1.0"]]
+         :injections [(require 'hashp.core)]}
+ :user [:shared :global-opts :jvm-opts :injected-tools :hashp]}
